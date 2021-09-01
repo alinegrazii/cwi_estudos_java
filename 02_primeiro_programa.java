6226374fbff19662a77e5b77a5181773be5c07ab
Aline Grazieli <alinegrazii@hotmail.com>
2021-09-01 13:45:53 -0300

    Create 02_primeiro_programa.java

---
+++ 02_primeiro_programa.java
@@ -0,0 +1,22 @@
+/*Crie um arquivo chamado AlunoReset.java
+Implemente um programa que imprima na tela o texto “Sou aluno do Level 1 do CWI Reset!”
+Compile e execute o programa*/
+
+class AlunoReset {
+
+    public static void main(String[] args) {
+        System.out.println("Sou aluno do Level 1 do CWI Reset!");
+    }
+}
+
+//salvar como AlunoReset.java
+
+//compilar
+cd //colar o diretorio onde salvou o arquivo
+javac AlunoReset.java
+
+//executar
+java AlunoReset
+
+//resultado output na tela
+Sou aluno do Level 1 do CWI Reset!
