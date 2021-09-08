ae00df95998981d3b6e634a8c6b93fa423ed798b
Aline Grazieli <alinegrazii@hotmail.com>
2021-09-08 14:38:51 -0300

    Create 05_variaveis.java

---
+++ 05_variaveis.java
@@ -0,0 +1,13 @@
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		int idade = 29;
+		String nome = "ALINE";
+		String sobrenome = "MACHADO";
+		
+		System.out.println("Nome: " + nome + " Sobrenome: " + sobrenome + " Idade: " + idade);
+		
+	}
+
+}
